6edc99144bba3812af62e2d9a77c4d3ff28ffcd0
stuntmanmike87 <mich.perigaud@gmail.com>
2024-05-11 17:59:27 +0200

    Add and provide fixtures

--- app/src/DataFixtures/PostsFixtures.php
+++ app/src/DataFixtures/PostsFixtures.php
@@ -1,19 +1,35 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\DataFixtures;
 
+use App\Entity\Posts;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
-final class PostsFixtures extends Fixture
+class PostsFixtures extends Fixture implements DependentFixtureInterface
 {
+    public function __construct(private readonly SluggerInterface $slugger){}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $newPost = new Posts();
+        $newPost->setUsers($this->getReference('Admin'));
+        $newPost->setTitle('Ceci est un titre');
+        $newPost->setSlug(strtolower($this->slugger->slug($newPost->getTitle())));
+        $newPost->setContent('Ceci est le contenu');
+        $newPost->setFeaturedImage('default.webp');
+        
+        $manager->persist($newPost);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UsersFixtures::class
+        ];
     }
 }
--- app/src/DataFixtures/UsersFixtures.php
+++ app/src/DataFixtures/UsersFixtures.php
@@ -1,18 +1,37 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\DataFixtures;
 
+use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-final class UsersFixtures extends Fixture
+class UsersFixtures extends Fixture
 {
+    public function __construct(private readonly UserPasswordHasherInterface $hasher){}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $newUser = new Users();
+
+        $newUser->setNickname('User');
+        $newUser->setPassword($this->hasher->hashPassword($newUser, 'azerty'));
+        $newUser->setEmail('user@demo.fr');
+        $newUser->setIsVerified(true);
+
+        $manager->persist($newUser);
+
+        $newUser = new Users();
+
+        $newUser->setNickname('Admin');
+        $newUser->setPassword($this->hasher->hashPassword($newUser, 'azerty'));
+        $newUser->setEmail('admin@demo.fr');
+        $newUser->setIsVerified(true);
+        $newUser->setRoles(['ROLE_ADMIN']);
+        $this->setReference('Admin', $newUser);
+
+        $manager->persist($newUser);
 
         $manager->flush();
     }
