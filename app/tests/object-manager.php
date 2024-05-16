34f5e3982bbc693353f139c8427d9b54b56674b8
stuntmanmike87 <mich.perigaud@gmail.com>
2024-05-16 09:35:34 +0200

    Improve PHPStan settings

---
+++ app/tests/object-manager.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Kernel;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__).'/vendor/autoload.php';//require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');//(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+//Usage of super global $_SERVER found; Usage of GLOBALS are discouraged
+//consider not relying on global scope
+
+$kernel->boot();
+
+///** @var ManagerRegistry $doctrine */
+//return $kernel->getContainer()->get('doctrine')/* ->getManager() */;
+
+/** @var ManagerRegistry $doctrine */
+$doctrine = $kernel->getContainer()->get('doctrine');
+
+return $doctrine->getManager();
