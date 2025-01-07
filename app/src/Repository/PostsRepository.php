56964fcf147aee49913d18b0e8c4ed5cff052b9e
stuntmanmike87 <mich.perigaud@gmail.com>
2025-01-07 10:45:56 +0100

    List of articles with pagination [sass-watch not processed]

--- app/src/Repository/PostsRepository.php
+++ app/src/Repository/PostsRepository.php
@@ -6,21 +6,33 @@ namespace App\Repository;
 
 use App\Entity\Posts;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Posts>
- *
- * @method Posts|null find($id, $lockMode = null, $lockVersion = null)
- * @method Posts|null findOneBy(array $criteria, array $orderBy = null)
- * @method Posts[]    findAll()
- * @method Posts[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 final class PostsRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Posts::class);
+    }
+
+    public function getAllPaginated(int $page = 1, int $limit = 10): array
+    {
+        $offset = ($page - 1) * $limit;
+        $query = $this->createQueryBuilder('p')
+            ->orderBy('p.id', 'DESC')
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+            
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+        $result['posts'] = $data;
+        $result['pages'] = ceil($paginator->count() / $limit);
+        $result['current'] = $page;
+        return $result;
     }
 
     //    /**
