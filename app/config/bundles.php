62ef165d7dca3d514e252bc595eecba1330f13fb
stuntmanmike87 <mich.perigaud@gmail.com>
2025-04-16 13:21:43 +0200

    Update deps 20250416

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
